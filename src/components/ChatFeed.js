f1f05b5e8c9dc3651913433415de68a3dbf8bf66
SAEBOMPAK <saebompak@gmail.com>
2021-12-29 15:44:58 -0800

    add renderMessage()

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -5,8 +5,36 @@ import TheirMessage from "./TheirMessage";
 const ChatFeed = (props) => {
     const {chats, activeChat, userName, messages } = props;
     const chat = chats && chats[activeChat]
-    //chats이 있을때 
-    console.log('here', chat,userName, messages )
+    //chats이 있을때 chats[activeChat] 찾음
+
+    const renderMessage = () => {
+        const keys = Object.keys(messages)
+        // keys 는 message의 specific message임
+
+        return keys.map((key,index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1]
+            //만약 거기 message가 있으면 맨 마지막것을 찾아라
+            const isMyMessage = userName === message.sender.username
+
+            return (
+                <div key={`msg_${index}`} style={{width:'100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ?
+                            <MyMessage />
+                            :
+                            <TheirMessage />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>read-receipts</div>
+                </div>
+            )
+        })
+    }
+    renderMessage();
+
     return (
         <div>
             ChatFeed
