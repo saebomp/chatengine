9a3ceadcf9e7f8e3cfaa3adfd6d4e565010f668f
SAEBOMPAK <saebompak@gmail.com>
2021-12-31 18:54:08 -0800

    uploading images

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import {sendMessage, isTyping} from 'react-chat-engine'
+import {SendOutlined, PictureOutlined} from '@ant-design/icons'
 
 const MessageForm = (props) => {
     const [value, setValue] = useState('')
@@ -11,6 +12,7 @@ const MessageForm = (props) => {
         //trim: string 의 white space 같은거 없앰
 
         if(text.length > 0) sendMessage(creds, chatId, {text});
+        setValue('')
     }
 
     const handleChange = (event) => {
@@ -18,6 +20,11 @@ const MessageForm = (props) => {
         isTyping(props.chatId)
             
     }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, {files: event.target.files, text:''})
+    }
+
 
     return (
         
@@ -30,6 +37,21 @@ const MessageForm = (props) => {
                     onChange={handleChange}
                     onSubmit={handleSubmit}
                 />
+                <label html for="upload-button">
+                    <span className='image-button'>
+                        <PictureOutlined className="picture-icon" />
+                    </span>
+                </label>
+                <input 
+                    type="file"
+                    multiple={false}
+                    id="upload-button"
+                    style={{display:'none'}}
+                    onChange={handleUpload}
+                />
+                <button type="submit" className='send-button'>
+                    <SendOutlined className="send-icon" />
+                </button>
             </form>
         </div>
     )
